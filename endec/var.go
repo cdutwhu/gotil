f6fc12ae4a7db707fc45bf33eaeea45553d7dcc7
Qing Miao <cdutwhu@outlook.com>
2020-07-13 16:30:40 +1000

    + endec some regular expressing

--- endec/var.go
+++ endec/var.go
@@ -2,14 +2,23 @@ package endec
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/cdutwhu/debog/fn"
 )
 
 var (
-	fPf  = fmt.Printf
-	fSf  = fmt.Sprintf
-	fPln = fmt.Println
-
+	fPf       = fmt.Printf
+	fSf       = fmt.Sprintf
+	fPln      = fmt.Println
 	failOnErr = fn.FailOnErr
+)
+
+var (
+	// RegexpMD5 : regular expression for MD5 string
+	RegexpMD5 = regexp.MustCompile("\"[A-Fa-f0-9]{32}\"")
+	// RegexpSHA1 : regular expression for SHA1 string
+	RegexpSHA1 = regexp.MustCompile("\"[A-Fa-f0-9]{40}\"")
+	// RegexpSHA256 : regular expression for SHA256 string
+	RegexpSHA256 = regexp.MustCompile("\"[A-Fa-f0-9]{64}\"")
 )
