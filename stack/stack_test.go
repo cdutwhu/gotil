4cecd60ddb12d9b3aba21db91a65311d64162156
Qing Miao <cdutwhu@outlook.com>
2020-11-29 17:35:00 +1100

    + stack

---
+++ stack/stack_test.go
@@ -0,0 +1,29 @@
+package stack
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	var s4s stack
+	s4s.push("abc")
+	s4s.push("def")
+	s4s.push("ghi")
+
+	fPln(s4s.sprint("-"))
+
+	val, ok := s4s.pop()
+	fPln(val, ok, s4s)
+	fPln(s4s.sprint("-"))
+
+	val, ok = s4s.pop()
+	s4s.push("jkl")
+	fPln(val, ok, s4s)
+	fPln(s4s.sprint("-"))
+
+	val, ok = s4s.pop()
+	fPln(val, ok, s4s)
+	fPln(s4s.sprint("-"))
+
+	val, ok = s4s.pop()
+	fPln(val, ok, s4s)
+	fPln(s4s.sprint("-"))
+}
