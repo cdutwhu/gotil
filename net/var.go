6caa902f95b46c6b2c103dba2d0e2934d8c0f032
Qing Miao <cdutwhu@outlook.com>
2021-02-24 17:35:09 +1100

    * net add url-params-func

--- net/var.go
+++ net/var.go
@@ -2,12 +2,17 @@ package net
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/cdutwhu/debog/fn"
 )
 
 var (
-	fSf       = fmt.Sprintf
-	fPln      = fmt.Println
-	warnOnErr = fn.WarnOnErr
+	fSf         = fmt.Sprintf
+	fPln        = fmt.Println
+	sHasSuffix  = strings.HasSuffix
+	sIndex      = strings.Index
+	sTrimRight  = strings.TrimRight
+	sTrimSuffix = strings.TrimSuffix
+	warnOnErr   = fn.WarnOnErr
 )
